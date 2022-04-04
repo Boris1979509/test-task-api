c3595afe6a28ef95b405ea3f3b78559334a07766
Boris <bobabonanadz@mail.ru>
2022-04-04 11:29:12 +0300

    Cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:8080', 'boris1979509.github.io'],
+    'allowed_origins' => ['http://localhost:8080', 'https://boris1979509.github.io'],
 
     'allowed_origins_patterns' => [],
 
